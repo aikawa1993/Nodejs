361f6cd50a67d72aecc609ffbc5ac05b71148c85
aikawa1993 <aikawa_kajuhiko34@yahoo.com>
2018-02-13 11:36:36 +0700

    ~commit

--- app.js
+++ app.js
@@ -37,53 +37,10 @@ app.use("/bs",express.static(__dirname + "/bootstrap/css"));
 app.use("/hinh",express.static(__dirname + "/img"));
 
 app.set("view engine","ejs");
-
-passport.use('local', new LocalStrategy({
-
-    usernameField: 'username',
   
-    passwordField: 'password',
-  
-    passReqToCallback: true //passback entire req to call back
-  } , function (req, username, password, done){
-  
-        if(!username || !password ) { return done(null, false, req.flash('message','All fields are required.')); }
-  
-        var salt = '7fa73b47df808d36c5fe328546ddef8b9011b2c6';
-  
-        connection.query("select * from Admin where username = ?", [username], function(err, rows){
-  
-            console.log(err); console.log(rows);
-  
-          if (err) return done(req.flash('message',err));
-  
-          if(!rows.length){ return done(null, false, req.flash('message','Invalid username or password.')); }
-  
-          salt = salt + '' + password;
-  
-          var encPassword = crypto.createHash('sha1').update(salt).digest('hex');
-
-          var dbPassword  = rows[0].password;
-  
-          if(!(dbPassword == encPassword)){ 
-              return done(null, false, req.flash('message','Invalid username or password.')); 
-           }
-  
-          return done(null, rows[0]);  
-        });
-    
-      }
-      
-  ));
-
-  app.post('/signup', passport.authenticate('local', {
-    successRedirect : '/user', // redirect to the secure profile section
-    failureRedirect : '/login', // redirect back to the signup page if there is an error
-    failureFlash : true // allow flash messages
-}));
-  
-apiController(app);
-homeController(app);
+// apiController(app);
+// homeController(app);
+require('./routes.js')(app, passport);
 
 app.listen(port, function(){
     console.log("Sever is listening on PORT", port);
